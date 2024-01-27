2efdd03ff9fe1d20bd1ba52f2a2fb76b7fa23116
andreafrancioni <andreafrancioni.work@gmail.com>
2024-01-27 11:06:56 +0100

    Crea progetto, modello, seed e importa dati su db tabella comics

---
+++ database/seeders/ComicSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $array_comic = config("comics");
+
+        foreach ($array_comic as $comic) {
+            $new_comic = new Comic();
+            $new_comic->description = $comic["description"];
+            $new_comic->title = $comic["title"];
+            $new_comic->thumb = $comic["thumb"];
+            $new_comic->price = $comic["price"];
+            $new_comic->series = $comic["series"];
+            $new_comic->sale_date = $comic["sale_date"];
+            $new_comic->type = $comic["type"];
+            $new_comic->save();
+        }
+    }
+}
